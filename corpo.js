68832882149b97a30cdaa1d0f4019af3296dca94
Jd Ramirez <jdr@ocadu.ca>
2025-11-26 19:34:52 -0500

    stuff

--- corpo.js
+++ corpo.js
@@ -1,17 +1,56 @@
-let currentVideo = 1;
-
-let video1, video2, video3;
 
 
 
-function setup() {
+// window.addEventListener('load',() => {
+//     const video = document.getElementById('video1');
+//     video.play();
+// });
 
-}
+window.addEventListener('scroll',() => {
+    const video1 = document.getElementById('video1');
+    const video2 = document.getElementById('video2');
+    const video3 = document.getElementById('video3');
 
-function preload() {
+    const headcontainer1 = document.querySelector('.headcontainer1');
+    const headcontainer2 = document.querySelector('.headcontainer2');
+    const headcontainer3 = document.querySelector('.headcontainer3');
+    
+    // If scrolled down more than 100px, show video2
+    console.log(window.scrollY);
 
 
-}
-function draw() {
+    if (window.scrollY < 10) {
+        headcontainer1.style.display = 'block';
+        headcontainer2.style.display = 'none';
+        headcontainer3.style.display = 'none';
+        // video1.style.position = 'sticky';
+        video1.style.display = 'block';
+        video2.style.display = 'none';
+        video3.style.display = 'none';
+        video1.play();
+    }
+    else if (window.scrollY <= 100) {
+     
+        headcontainer1.style.display = 'none';
+        headcontainer2.style.display = 'block';
+        headcontainer3.style.display = 'none';
 
-}
+        // video2.style.position = 'sticky'; 
+        video1.style.display = 'none';
+        video2.style.display = 'block';
+        video3.style.display = 'none';
+        video2.play();
+    }
+    else if (window.scrollY <= 200) {
+
+        headcontainer1.style.display = 'none';
+        headcontainer2.style.display = 'none';
+        headcontainer3.style.display = 'block';
+        // video3.style.position = 'sticky';
+
+        video1.style.display = 'none';
+        video2.style.display = 'none';
+        video3.style.display = 'block';
+        video3.play();
+    }
+});
